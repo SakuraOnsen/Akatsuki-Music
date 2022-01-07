f1550cab34fb128a5de32726030ec4e77a9d6396
Ujaan Mukherjee <ujaanmukhopadhyay@gmail.com>
2022-01-08 01:01:28 +0530

    play base create

--- commands/configurations/ping.js
+++ commands/configurations/ping.js
@@ -11,10 +11,12 @@ module.exports = {
 
   execute: async (Discord, client, message, args, InvalidUsage, MsgError) => {
 
+    
+
     message.channel.send({
       embeds: [
         new Discord.MessageEmbed()
-          .setAuthor('Pong', client.user.displayAvatarURL())
+          .setAuthor(client.user.tag, client.user.displayAvatarURL())
           .setThumbnail(client.user.displayAvatarURL())
           .setDescription(`\`\`\`apache\nLatency_Ping: ${client.ws.ping}ms\nGateway_Ping: ${Date.now() - message.createdTimestamp}ms\`\`\``)
           .setTimestamp(Date.now())
---
+++ commands/music/play.js
@@ -0,0 +1,16 @@
+    module.exports = {
+    name: 'play',
+    aliases: ['p'],
+    permissions: ['CONNECT', 'SPEAK'],
+    cooldown: null,
+    argsreq: 1,
+    usage: '+play [song name]',
+    description: 'Eval Code',
+    category: 'dev',
+    dev: true,
+
+    execute: async (Discord, client, message, args, InvalidUsage, MsgError) => {
+
+        
+    }
+}
