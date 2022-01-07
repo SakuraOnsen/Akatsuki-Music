237b6e29abbce5d5cfbd2449ee1712fd6bf772e8
Ujaan Mukherjee <ujaanmukhopadhyay@gmail.com>
2022-01-07 23:26:37 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const { Client, Collection, Intents } = require(`discord.js`);
 const Discord = require('discord.js')
 const { readdirSync } = require('fs');
 const { readFile, writeFile } = require('fs').promises;
-const express = require('express')
-const port = 3000
-const app = express()
-
 
 const client = new Client({ intents: 32767 })
 module.exports = client;
 client.commands = new Collection();
 client.aliases = new Collection();
 client.author = ['722302164302692472']
-client.snipes = new Collection();
 
 const handlers = ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
