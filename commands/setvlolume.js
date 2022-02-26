55da2005b8b05d006ef2848952c797348896c2d4
KireinaR <ujaanmukhopadhyay@gmail.com>
2022-02-26 18:40:00 +0530

    I LOVE ZERO TWO

--- commands/setvlolume.js
+++ commands/setvlolume.js
@@ -2,58 +2,63 @@ const { MessageEmbed } = require("discord.js");
 const { webhookClient } = require("../index");
 
 module.exports = {
-    name: "setvolume",
-    aliases: ['volume', 'v', 'sv'],
-    /**
-     *
-     * @param {Client} client
-     * @param {Message} message
-     * @param {String[]} args
-     */
-    run: async (client, message, args) => {
+  name: "setvolume",
+  aliases: ["volume", "v", "sv"],
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    try {
+      const VoiceChannel = message.member.voice.channel;
+      if (!VoiceChannel)
+        return message.channel.send({
+          embeds: [
+            new MessageEmbed()
+              .setColor("RED")
+              .setDescription(
+                "You need to be in a voice channel to use music commands!"
+              ),
+          ],
+        });
 
-        try {
+      if (
+        message.guild.me.voice.channelId &&
+        VoiceChannel.id !== message.guild.me.voice.channelId
+      )
+        return message.channel.send({
+          embeds: [
+            new MessageEmbed()
+              .setColor("RED")
+              .setDescription("I am already playing in another channel!"),
+          ],
+        });
 
-            const VoiceChannel = message.member.voice.channel;
-            if (!VoiceChannel) return message.channel.send({
-                embeds: [
-                    new MessageEmbed()
-                        .setColor("RED")
-                        .setDescription("You need to be in a voice channel to use music commands!")
-                ]
-            })
+      const queue = client.distube.getQueue(message);
+      const vol = Number(args[0]);
+      //const VoiceChannel = message.member.voice.channel;
+      await queue.setVolume(vol);
+      message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("BLURPLE")
+            .setDescription(`🔊 Volume set to to \`${vol}%\``),
+        ],
+      });
+    } catch (e) {
+      const errEmbed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`${e}`);
 
-            if (message.guild.me.voice.channelId && VoiceChannel.id !== message.guild.me.voice.channelId) return message.channel.send({
-                embeds: [
-                    new MessageEmbed()
-                        .setColor("RED")
-                        .setDescription("I am already playing in another channel!")
-                ]
-            })
-
-            const queue = client.distube.getQueue(message)
-            const vol = Number(args[0])
-            //const VoiceChannel = message.member.voice.channel;
-            await queue.setVolume(vol)
-            message.channel.send({
-                embeds: [
-                    new MessageEmbed()
-                        .setColor("BLURPLE")
-                        .setDescription(`🔊 Volume set to to \`${vol}%\``)
-                ]
-            })
-
-        } catch (e) {
-            const errEmbed = new MessageEmbed()
-                .setColor("RED")
-                .setDescription(`${e}`)
-
-            webhookClient.send({
-                content: 'Error',
-                username: 'Akatuki Music',
-                avatarURL: 'https://images-ext-1.discordapp.net/external/RluYBkBAf4G7C2L8nzHoXGwC70iFfVOxrrsfdZXOrrk/%3Fsize%3D256/https/cdn.discordapp.com/avatars/928941386441564211/74aac125f520e94357317db44bfcccd2.png',
-                embeds: [errEmbed],
-            });
-        }
-    },
+      webhookClient.send({
+        content: "Error",
+        username: "Akatuki Music",
+        avatarURL:
+          "https://images-ext-1.discordapp.net/external/RluYBkBAf4G7C2L8nzHoXGwC70iFfVOxrrsfdZXOrrk/%3Fsize%3D256/https/cdn.discordapp.com/avatars/928941386441564211/74aac125f520e94357317db44bfcccd2.png",
+        embeds: [errEmbed],
+      });
+    }
+  },
 };
